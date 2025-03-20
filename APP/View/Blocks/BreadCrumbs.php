e8772474e7f84ddf83afbd108c6fca3bb6174409
ElenaDz <elena-gosu@mail.ru>
2025-03-20 15:02:56 +0300

    update

--- APP/View/Blocks/BreadCrumbs.php
+++ APP/View/Blocks/BreadCrumbs.php
@@ -5,10 +5,12 @@ if (_isUrl(\APP\Action\Index::getUrl()))
     //return;
 }
 ?>
-<nav aria-label="breadcrumb">
-	<ol class="breadcrumb">
-		<li class="breadcrumb-item"><a href="#">Главная</a></li>
-		<li class="breadcrumb-item"><a href="#">Каталог</a></li>
-		<li class="breadcrumb-item active" aria-current="page">Данные</li>
-	</ol>
-</nav>
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Главная</a></li>
+            <li class="breadcrumb-item"><a href="#">Каталог</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Данные</li>
+        </ol>
+    </nav>
+</div>
--- APP/View/Blocks/ToolBar.php
+++ APP/View/Blocks/ToolBar.php
@@ -4,18 +4,18 @@
 
 		<img class="logo" src="/assets/img/logo.png" alt="<?= \APP\Config\Main::getTitle(); ?>">
 
-		<form class="d-flex" role="search">
+		<form class="d-flex w-100" role="search">
 			<input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Поиск">
 			<button class="btn btn-outline-success" type="submit">Поиск</button>
 		</form>
 
 	</div>
 
-	<div class="w-100 d-flex align-items-center">
+	<div class="info w-100 d-flex align-items-center">
 		<div>Локация </div>
 		<div>Телефоны, обратный звонок</div>
-		<div>Почта</div>
-		<div>Корзина?</div>
+		<div><a class="" href="#"><i class="mail bi"></i></a></div>
+		<div><a class="" href="#"><i class="basket bi"></i></a></div>
 	</div>
 
 </div>
