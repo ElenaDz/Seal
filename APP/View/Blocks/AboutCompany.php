73d7effa0c81ff38401cd3984c6ff8ab28b53ee0
ElenaDz <elena-gosu@mail.ru>
2025-03-26 19:47:38 +0300

    update

--- APP/View/Blocks/AboutCompany.php
+++ APP/View/Blocks/AboutCompany.php
@@ -1,6 +1,6 @@
 <div class="">
     <div class="container ">
-        <div class=" bg-white">
+        <div class=" bg-white shadow p-3 mb-5 bg-white rounded">
             <div class="col-12 col-lg-6 margin_unset">
                 <h1 class="h3 montserat"><b>О компании</b></h1>
                 <p class="ptsans">ТЕХНОМАРКЕТ — ПРОИЗВОДСТВО БЫТОВЫХ МОДУЛЕЙ, СООРУЖЕНИЙ, БЫСТРОВОЗВОДИМЫХ ЗДАНИЙ И КОМПЛЕКСНОЕ ОБЕСПЕЧЕНИЕ ТОВАРАМИ</p>
--- APP/View/Blocks/ListProduct.php
+++ APP/View/Blocks/ListProduct.php
@@ -1,32 +1,30 @@
 <div>
     <div class="container">
-        <div class="bg-white">
-<!--            ссылка на страницу ЗПУ-->
-            <a href="#" class="link-dark d-flex justify-content-center"><h2 >ЗПУ</h2></a>
+        <div class="">
             <div class="card-group">
 <!--                Каждая карточкка это ссылка на страницу товара-->
-                <div class="card">
+                <div class="card shadow p-3 mb-5 bg-white rounded">
                     <img src="/assets/img/seal/tp350-01.jpg" class="card-img-top" alt="tp350-01">
                     <div class="card-body">
                         <h5 class="card-title">ТП 350-01</h5>
                         <p class="card-text">Описание.</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card shadow p-3 mb-5 bg-white rounded">
                     <img src="/assets/img/seal/tp40.jpg" class="card-img-top" alt="tp40">
                     <div class="card-body">
                         <h5 class="card-title">ТП 40 Закрутка</h5>
                         <p class="card-text">Описание</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card shadow p-3 mb-5 bg-white rounded">
                     <img src="/assets/img/seal/tp1200-01.jpg" class="card-img-top" alt="tp1200-01">
                     <div class="card-body">
                         <h5 class="card-title">ТП 1200-01</h5>
                         <p class="card-text">Описание</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card shadow p-3 mb-5 bg-white rounded">
                     <img src="/assets/img/seal/tp2800-02.jpg" class="card-img-top" alt="tp2800-02">
                     <div class="card-body">
                         <h5 class="card-title">ТП 2900-02</h5>
