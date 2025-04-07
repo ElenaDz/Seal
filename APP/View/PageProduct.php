15d7f3aed39a491c8d626792ff41fe8df06563cc
ElenaDz <elena-gosu@mail.ru>
2025-04-07 18:48:08 +0300

    update page product

--- APP/View/PageProduct.php
+++ APP/View/PageProduct.php
@@ -3,39 +3,59 @@ use APP\Entity\Product;
 
 /** @var Product $product */
 ?>
-<div class="container  bg-white shadow p-3 mb-5 bg-white rounded">
+<div class="container  page_product bg-white shadow p-3 mb-5 bg-white rounded">
     <div class="d-flex">
-        <div>
+        <div class="cover">
             <img src="/assets/img/seal/<?= $product->getCover()?>.jpg" width="100%" height="100%"  alt="">
         </div>
-        <div>
-            <div><?= $product->getTitle()?></div>
-            <div>
-                <h5>Технические характеристики</h5>
+        <div class="characteristics_wrap">
+            <div><h2><?= $product->getTitle()?></h2></div>
+            <div >
+                <h5 class="mb-2">Технические характеристики</h5>
+                <ul class="p-0">
+                    <li class="d-flex">
+                        <div class="element">Тип:</div>
+                        <div class="characteristics">Универсальные ЗПУ;</div>
+                    </li>
+                    <li class="d-flex">
+                        <div class="element">Разрушающее усилие при растяжении замкнутой петли каната:</div>
+                        <div class="characteristics">не менее 3,5 кН;</div>
+                    </li>
+                    <li class="d-flex">
+                        <div class="element">Диаметр гибкого стержня (каната):</div>
+                        <div class="characteristics"> 2,2 мм;</div>
+                    </li>
+                    <li class="d-flex">
+                        <div class="element">Длина каната: </div>
+                        <div class="characteristics">500 мм;</div>
+                    </li>
+                    <li class="d-flex">
+                        <div class="element">Масса:</div>
+                        <div class="characteristics"> не более 30 г;</div>
+                    </li>
+                    <li class="d-flex">
+                        <div class="element">Усилие замыкания стержня в пломбе:</div>
+                        <div class="characteristics">не более 60 Н;</div>
+                    </li>
 
-                <p>Тип: Универсальные ЗПУ;
-                Разрушающее усилие при растяжении замкнутой петли каната: не менее 3,5 кН;
-                Диаметр гибкого стержня (каната): 2,2 мм;
-                Длина каната: 500 мм;
-                Масса: не более 30 г;
-                Усилие замыкания стержня в пломбе: не более 60 Н;
-                </p>
+                </ul>
             </div>
         </div>
-
     </div>
-    <div>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-whatever="@fat">
+
+    <div class="btn_wrap">
+        <button type="button" class="btn  btn_base"  data-bs-toggle="modal" data-bs-target="#exampleModal" data-whatever="@fat">
             Заказать звонок
         </button>
-
     </div>
+
     <div>
         <div>
             <h5>Назначение, область применения</h5>
 
             <p>Запорно-пломбировочное устройство типа «ТП 350-01»  предназначено для пломбирования специализированных железнодорожных цистерн с диаметром пломбировочного отверстия не менее 2,5 мм для перевозки сжиженных газов, кислот и других химических продуктов, и подлежит строгому учету. </p>
         </div>
+
         <div>
             <h5>Достоинства</h5>
 
