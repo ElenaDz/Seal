c258075c12e3c39a3f050e654c6acf0ae88cc5b3
ElenaDz <elena-gosu@mail.ru>
2025-04-15 15:32:16 +0300

    update

--- APP/Helper/Email.php
+++ APP/Helper/Email.php
@@ -23,20 +23,19 @@ class Email
 		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 		$mail->Host       = 'smtp.yandex.ru';
 		// здесь логин от яндекса
-		$mail->Username   = 'Lenagosu';
+		$mail->Username   = 'test';
 		// завести новый пароль для приложения почты здесь https://passport.yandex.ru/profile/access использовать здесь
 		// старый пароль от яндекса при этом не измениться, здесь заводятся дополнительные пароли к основному, чтобы не светить основной
 		// для отладки подойдет твой, для релиза нужно будет попросить заказчика сделать это
 		// так же надо включить настройку "Разрешить доступ к почтовому ящику с помощью почтовых клиентов" в настройках яндекс почты
-		$mail->Password   = 'dmeiuzugqgameyar';
-		$mail->Password   = 'obalonapxnqcjskw';
+		$mail->Password   = 'Пароль приложения';
 		$mail->Port       = 587;
 
 		// здесь email кому отправлять это письмо например в нашем случае это может быть tehnomarket.nhk@yandex.ru
-		$mail->addAddress("Lenagosu@yandex.ru");
+		$mail->addAddress("test.ru");
 
 		// здесь указать email с того же аккаунта, что выше был указан пароль, может совпадать с email to
-		$mail->setFrom("Lenagosu@yandex.ru");
+		$mail->setFrom("test.ru");
 
 		$mail->isHTML(true);
 
